1df45343a7f06fcd5ea69f26f7286ea978adf01e
robertSahm <robertsahm@gmail.com>
2018-04-20 18:08:42 -0600

    hot fiya

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
         postCssPlugins: [
-          cssNext(), 
+          cssNext(),
           cssNest(),
           lost(),
         ],
